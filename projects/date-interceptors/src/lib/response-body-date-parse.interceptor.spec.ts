287450c602c4d3e7f39bbef21fce1009847fe276
SwabianCoder <43047586+SwabianCoder@users.noreply.github.com>
2022-08-13 15:35:06 +0000

    ✅ Added negative ResponseBodyDateParseInterceptor unit tests

--- projects/date-interceptors/src/lib/response-body-date-parse.interceptor.spec.ts
+++ projects/date-interceptors/src/lib/response-body-date-parse.interceptor.spec.ts
@@ -145,4 +145,204 @@ describe('ResponseBodyDateParseInterceptor', () => {
       return interceptResult;
     });
   }
+
+  for (const testCase of [
+    {
+      description: 'root object of array item',
+      expectedResponse: [
+        {
+          id: '9ed84cf1-9f77-4ce7-9b48-dc7b49d5801e',
+          lastName: 'someLastName',
+          birthday: '2020-02-02T12:45:30Z',
+        },
+      ],
+      httpResponse: [
+        {
+          id: '9ed84cf1-9f77-4ce7-9b48-dc7b49d5801e',
+          lastName: 'someLastName',
+          birthday: '2020-02-02T12:45:30Z',
+        },
+      ],
+    },
+    {
+      description: 'root object',
+      expectedResponse: {
+        id: '9ed84cf1-9f77-4ce7-9b48-dc7b49d5801e',
+        lastName: 'someLastName',
+        birthday: '2020-02-02T12:45:30Z',
+      },
+      httpResponse: {
+        id: '9ed84cf1-9f77-4ce7-9b48-dc7b49d5801e',
+        lastName: 'someLastName',
+        birthday: '2020-02-02T12:45:30Z',
+      },
+    },
+    {
+      description: 'nested object of array item',
+      expectedResponse: [
+        {
+          id: '9ed84cf1-9f77-4ce7-9b48-dc7b49d5801e',
+          lastName: 'someLastName',
+          inner: {
+            birthday: '2020-02-02T12:45:30Z',
+          },
+        },
+      ],
+      httpResponse: [
+        {
+          id: '9ed84cf1-9f77-4ce7-9b48-dc7b49d5801e',
+          lastName: 'someLastName',
+          inner: {
+            birthday: '2020-02-02T12:45:30Z',
+          },
+        },
+      ],
+    },
+    {
+      description: 'nested object',
+      expectedResponse: {
+        id: '9ed84cf1-9f77-4ce7-9b48-dc7b49d5801e',
+        lastName: 'someLastName',
+        inner: {
+          birthday: '2020-02-02T12:45:30Z',
+        },
+      },
+      httpResponse: {
+        id: '9ed84cf1-9f77-4ce7-9b48-dc7b49d5801e',
+        lastName: 'someLastName',
+        inner: {
+          birthday: '2020-02-02T12:45:30Z',
+        },
+      },
+    },
+  ]) {
+    // eslint-disable-next-line require-await
+    it(`does not parse date strings of HTTP response body of ${testCase.description} with wrong format`, async (): Promise<
+      HttpEvent<unknown>
+    > => {
+      dummyRequest.castToWritable().url = 'https://test-url.com/test';
+
+      const next = {
+        handle(
+          _request: HttpRequest<unknown>
+        ): Observable<HttpResponse<unknown>> {
+          const dummyResponse = new HttpResponse<unknown>({
+            status: 200,
+            body: testCase.httpResponse,
+          });
+
+          return of(dummyResponse);
+        },
+      };
+
+      const interceptResult$ = spectator.service.intercept(dummyRequest, next);
+      const interceptResult = await firstValueFrom(interceptResult$);
+
+      expect((interceptResult as HttpResponse<unknown>).body).toEqual(
+        testCase.expectedResponse
+      );
+
+      return interceptResult;
+    });
+  }
+
+  for (const testCase of [
+    {
+      description: 'root object of array item',
+      expectedResponse: [
+        {
+          id: '9ed84cf1-9f77-4ce7-9b48-dc7b49d5801e',
+          lastName: 'someLastName',
+          birthday: '2020-02-02T12:45:30.000Z',
+        },
+      ],
+      httpResponse: [
+        {
+          id: '9ed84cf1-9f77-4ce7-9b48-dc7b49d5801e',
+          lastName: 'someLastName',
+          birthday: '2020-02-02T12:45:30.000Z',
+        },
+      ],
+    },
+    {
+      description: 'root object',
+      expectedResponse: {
+        id: '9ed84cf1-9f77-4ce7-9b48-dc7b49d5801e',
+        lastName: 'someLastName',
+        birthday: '2020-02-02T12:45:30.000Z',
+      },
+      httpResponse: {
+        id: '9ed84cf1-9f77-4ce7-9b48-dc7b49d5801e',
+        lastName: 'someLastName',
+        birthday: '2020-02-02T12:45:30.000Z',
+      },
+    },
+    {
+      description: 'nested object of array item',
+      expectedResponse: [
+        {
+          id: '9ed84cf1-9f77-4ce7-9b48-dc7b49d5801e',
+          lastName: 'someLastName',
+          inner: {
+            birthday: '2020-02-02T12:45:30.000Z',
+          },
+        },
+      ],
+      httpResponse: [
+        {
+          id: '9ed84cf1-9f77-4ce7-9b48-dc7b49d5801e',
+          lastName: 'someLastName',
+          inner: {
+            birthday: '2020-02-02T12:45:30.000Z',
+          },
+        },
+      ],
+    },
+    {
+      description: 'nested object',
+      expectedResponse: {
+        id: '9ed84cf1-9f77-4ce7-9b48-dc7b49d5801e',
+        lastName: 'someLastName',
+        inner: {
+          birthday: '2020-02-02T12:45:30.000Z',
+        },
+      },
+      httpResponse: {
+        id: '9ed84cf1-9f77-4ce7-9b48-dc7b49d5801e',
+        lastName: 'someLastName',
+        inner: {
+          birthday: '2020-02-02T12:45:30.000Z',
+        },
+      },
+    },
+  ]) {
+    // eslint-disable-next-line require-await
+    it(`does not parse date strings of HTTP response body of ${testCase.description} with wrong HTTP request URL`, async (): Promise<
+      HttpEvent<unknown>
+    > => {
+      dummyRequest.castToWritable().url = 'https://demo-url.com/test';
+
+      const next = {
+        handle(
+          _request: HttpRequest<unknown>
+        ): Observable<HttpResponse<unknown>> {
+          const dummyResponse = new HttpResponse<unknown>({
+            status: 200,
+            body: testCase.httpResponse,
+          });
+
+          return of(dummyResponse);
+        },
+      };
+
+      const interceptResult$ = spectator.service.intercept(dummyRequest, next);
+      const interceptResult = await firstValueFrom(interceptResult$);
+
+      expect((interceptResult as HttpResponse<unknown>).body).toEqual(
+        testCase.expectedResponse
+      );
+
+      return interceptResult;
+    });
+  }
 });
