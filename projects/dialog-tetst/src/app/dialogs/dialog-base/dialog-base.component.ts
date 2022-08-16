a57d91349acd329bb31a6d2f969c1a1e2e8754b6
SwabianCoder <43047586+SwabianCoder@users.noreply.github.com>
2022-08-16 17:41:27 +0000

    🔥 Removed lazy dialogs test project

--- projects/dialog-tetst/src/app/dialogs/dialog-base/dialog-base.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-dialog-base',
-  templateUrl: './dialog-base.component.html',
-  styleUrls: ['./dialog-base.component.scss'],
-})
-export class DialogBaseComponent {}
