ee824d02b7342d7ed8e90d0a7c27ff5a67a56c90
SwabianCoder <43047586+SwabianCoder@users.noreply.github.com>
2022-08-16 10:26:54 +0000

    🚧 Got rid of obsolete Angular function in lazy dialog service

--- projects/lazy-dialogs/src/lib/services/lazy-dialog.service.ts
+++ projects/lazy-dialogs/src/lib/services/lazy-dialog.service.ts
@@ -1,17 +1,14 @@
 import { DOCUMENT } from '@angular/common';
 import {
-  ApplicationRef,
-  ComponentFactoryResolver,
   createNgModule,
-  EmbeddedViewRef,
   Inject,
   Injectable,
   Injector,
-  NgModule,
   NgModuleRef,
   Renderer2,
   RendererFactory2,
   Type,
+  ViewContainerRef,
 } from '@angular/core';
 import { LazyDialog, LazyDialogRef, ModuleWithLazyDialog } from '../models';
 
@@ -22,14 +19,13 @@ export class LazyDialogService {
   public constructor(
     @Inject(DOCUMENT) private readonly document: Readonly<Document>,
     private readonly rendererFactory: RendererFactory2,
-    private readonly applicationRef: ApplicationRef,
-    private readonly injector: Injector,
-    private readonly componentFactoryResolver: ComponentFactoryResolver
+    private readonly injector: Injector
   ) {
     this.renderer = this.rendererFactory.createRenderer(null, null);
   }
 
   public async create<ComponentType extends LazyDialog, DataType>(
+    viewContainerRef: ViewContainerRef,
     module: Promise<Type<ModuleWithLazyDialog<ComponentType> | ComponentType>>,
     params?: DataType
   ): Promise<LazyDialogRef<ComponentType, DataType>> {
@@ -45,9 +41,7 @@ export class LazyDialogService {
       component = resolvedModule as Type<ComponentType>;
     }
 
-    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(component);
-    const dialogComponentRef = componentFactory.create(this.injector);
-    this.applicationRef.attachView(dialogComponentRef.hostView);
+    const dialogComponentRef = viewContainerRef.createComponent(component);
     this.renderer.appendChild(
       this.document.body,
       dialogComponentRef.location.nativeElement
