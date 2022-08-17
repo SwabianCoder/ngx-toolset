334130bea1a12ee07a5748ff55a8c70f13d182a3
SwabianCoder <43047586+SwabianCoder@users.noreply.github.com>
2022-08-17 06:59:27 +0000

    ✅ Further improvements for lazy dialog service unit tests

--- projects/lazy-dialogs/src/lib/models/lazy-dialog-ref.spec.ts
+++ projects/lazy-dialogs/src/lib/models/lazy-dialog-ref.spec.ts
@@ -1,1 +1,7 @@
-// TODO: test LazyDialogRef close(...) and onClose()
+/*import { LazyDialogRef } from "./lazy-dialog-ref";
+
+describe('LazyDialogRef', () => {
+  //const lazyDialogRef = new LazyDialogRef();
+
+  // TODO: test LazyDialogRef close(...) and onClose()
+});*/
--- projects/lazy-dialogs/src/lib/services/lazy-dialog.service.spec.ts
+++ projects/lazy-dialogs/src/lib/services/lazy-dialog.service.spec.ts
@@ -1,5 +1,7 @@
-import { Component, NgModule } from '@angular/core';
-import { SpectatorService, createServiceFactory } from '@ngneat/spectator';
+import { DOCUMENT } from '@angular/common';
+import { ApplicationRef, Component, ComponentRef, createNgModule, ElementRef, EnvironmentInjector, Injector, NgModule, NgModuleRef, Renderer2, RendererFactory2, RendererType2, Type, ViewContainerRef } from '@angular/core';
+import { SpectatorService, createServiceFactory, createSpyObject, SpyObject } from '@ngneat/spectator';
+import { LazyDialogContainerComponent } from '../components';
 import { LAZY_DIALOG_CONTAINER_STYLES } from '../injection-tokens';
 import { LazyDialogRef, ModuleWithLazyDialog } from '../models';
 import { LazyDialogService } from './lazy-dialog.service';
@@ -55,6 +57,30 @@ export class DialogWithInvalidModuleComponent {
 })
 export class DialogWithInvalidModuleComponentModule {}
 
+const mockRenderer = {
+  createElement: function(name: string, namespace?: string | null | undefined): any {
+    return document.createElement('div');
+  },
+  addClass:function(el: any, name: string): void{},
+  appendChild:function(parent: any, newChild: any): void{}
+} as Renderer2;
+
+const mockComponentRef = {
+  instance: {
+    dialogContainer: {
+      createComponent: function<C>(componentType: Type<C>, options?: {
+          index?: number;
+          injector?: Injector;
+          ngModuleRef?: NgModuleRef<unknown>;
+          environmentInjector?: EnvironmentInjector | NgModuleRef<unknown>;
+          projectableNodes?: Node[][];
+      }): ComponentRef<C> {
+        return {} as ComponentRef<C>;
+      },
+    }
+  }
+} as unknown as ComponentRef<any>;
+
 describe('LazyDialogService', () => {
   let spectator: SpectatorService<LazyDialogService>;
   const createService = createServiceFactory({
@@ -64,16 +90,57 @@ describe('LazyDialogService', () => {
         provide: LAZY_DIALOG_CONTAINER_STYLES,
         useValue: {},
       },
-    ],
+      {
+        provide: RendererFactory2,
+        useValue: {
+          createRenderer: function(element: any, type: RendererType2 | null): Renderer2 {
+            return mockRenderer;
+          },
+        }
+      },
+      {
+        provide: ApplicationRef,
+        useValue: {
+          bootstrap: function<C>(component: Type<C>, rootSelectorOrNode?: string | any): ComponentRef<C> {
+            return mockComponentRef;
+          }
+        }
+      }
+    ]
   });
 
-  beforeEach(() => (spectator = createService()));
+  beforeEach(async () => {
+    spectator = createService();
+     applicationRef = spectator.inject(ApplicationRef);
+    injector = spectator.inject(Injector);
+
+     createElementSpy = spyOn(mockRenderer,'createElement').and.callThrough();
+     addClassSpy = spyOn(mockRenderer,'addClass').and.callThrough();
+     appendChildSpy = spyOn(mockRenderer,'appendChild').and.callThrough();
+     bootstrapSpy = spyOn(applicationRef, 'bootstrap').and.callThrough();
+     createComponentSpy = spyOn(mockComponentRef.instance.dialogContainer, 'createComponent').and.callThrough();
+
+     dialogContainerComponent = await import(
+      '../components/lazy-dialog-container/lazy-dialog-container.component'
+    ).then((m) => m.LazyDialogContainerComponent);
+    divElement = document.createElement('div');
+  });
+
+  let dialogContainerComponent: Type<LazyDialogContainerComponent>;
+  let applicationRef: SpyObject<ApplicationRef>;
+  let injector : SpyObject<Injector>;
+  let createElementSpy: jasmine.Spy;
+  let addClassSpy: jasmine.Spy;
+  let appendChildSpy:jasmine.Spy;
+  let bootstrapSpy:jasmine.Spy;
+  let createComponentSpy: jasmine.Spy;
+  let divElement: HTMLDivElement;
 
   it('creates instance', () => {
     expect(spectator.service).toBeTruthy();
   });
 
-  it('creates lazy dialog from standalone component with data', async (): Promise<void> => {
+  fit('creates lazy dialog from standalone component with data', async (): Promise<void> => {
     const creationResult = await spectator.service.create(
       Promise.resolve(StandaloneDialogComponent),
       {
@@ -81,6 +148,19 @@ describe('LazyDialogService', () => {
       }
     );
 
+    expect(createElementSpy).toHaveBeenCalledTimes(1);
+    expect(createElementSpy).toHaveBeenCalledWith('div');
+    expect(addClassSpy).toHaveBeenCalledTimes(1);
+    expect(addClassSpy).toHaveBeenCalledWith(divElement,'dialog-root');
+    expect(appendChildSpy).toHaveBeenCalledTimes(1);
+    expect(appendChildSpy).toHaveBeenCalledWith(document.body,divElement);
+    expect(bootstrapSpy).toHaveBeenCalledTimes(1);
+    expect(bootstrapSpy).toHaveBeenCalledWith(dialogContainerComponent, divElement);
+    expect(createComponentSpy).toHaveBeenCalledTimes(1);
+    expect(createComponentSpy).toHaveBeenCalledWith(StandaloneDialogComponent, {
+      injector,
+      ngModuleRef: undefined
+    });
     expect(creationResult).toBeTruthy();
     expect(creationResult.data).toBeTruthy();
     expect(creationResult.data).toEqual({ test: 'data' });
