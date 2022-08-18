10b514c204c6f67289ba907775252f69bc256359
SwabianCoder <43047586+SwabianCoder@users.noreply.github.com>
2022-08-18 08:09:39 +0000

    📝 Added JSDoc comments to template-type-checker project

--- projects/template-type-checker/src/lib/types/constructor.ts
+++ projects/template-type-checker/src/lib/types/constructor.ts
@@ -1,2 +1,9 @@
+/**
+ * Represents the class to validate the object against using {@link https://github.com/SwabianCoder/ngx-toolset/blob/main/projects/template-type-checker/src/lib/type-checker.pipe.ts TypeCheckerPipe}
+ *
+ * @export
+ * @typedef {Constructor}
+ * @template T
+ */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Constructor<T> = { new (...args: any[]): T };
