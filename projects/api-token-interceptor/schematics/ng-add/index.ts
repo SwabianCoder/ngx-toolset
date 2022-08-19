f8f74679e7b97feed91be80d3d7e67384208f66b
SwabianCoder <43047586+SwabianCoder@users.noreply.github.com>
2022-08-19 12:30:52 +0000

    🚨 Fixed api-token-interceptor linter issues

--- projects/api-token-interceptor/schematics/ng-add/index.ts
+++ projects/api-token-interceptor/schematics/ng-add/index.ts
@@ -4,6 +4,7 @@ import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 export function ngAdd(): Rule {
   return (tree: Tree, context: SchematicContext) => {
     context.addTask(new NodePackageInstallTask());
+
     return tree;
   };
 }
