b542e3a84dccf02832e9441ad79d10a7aadeaa0c
SwabianCoder <43047586+SwabianCoder@users.noreply.github.com>
2022-08-20 09:30:41 +0000

    🐛 Fixed ng add schematic of api-token-interceptor project

--- projects/api-token-interceptor/schematics/ng-add/index.ts
+++ projects/api-token-interceptor/schematics/ng-add/index.ts
@@ -6,81 +6,74 @@ import {
 } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 import { applyToUpdateRecorder } from '@schematics/angular/utility/change';
-import { addSymbolToNgModuleMetadata } from '../utility/ast-utils';
 import * as ts from 'typescript';
+import { addSymbolToNgModuleMetadata } from '../utility/ast-utils';
 
 export function ngAdd(): Rule {
   return (tree: Tree, context: SchematicContext) => {
-    addModuleAndInjectionTokens(tree, context);
-    installDependencies(context);
+    context.logger.info('Installing dependencies...');
+    context.addTask(new NodePackageInstallTask());
+
+    context.logger.info('Adding ApiTokenInterceptorModule to the app...');
+    const appModulePath = './src/app/app.module.ts';
+
+    if (!tree.exists(appModulePath)) {
+      throw new SchematicsException(
+        `The file ${appModulePath} doesn't exist...`
+      );
+    }
+
+    const recorder = tree.beginUpdate(appModulePath);
+    const appModuleFileContent = tree.read(appModulePath);
+
+    if (appModuleFileContent === null) {
+      throw new SchematicsException(
+        `The content of ${appModulePath} couldn't be read...`
+      );
+    }
+
+    const appModuleFileText = appModuleFileContent.toString('utf-8');
+    const sourceFile = ts.createSourceFile(
+      appModulePath,
+      appModuleFileText,
+      ts.ScriptTarget.Latest,
+      true
+    );
+    const importPath = '@ngx-toolset/api-token-interceptor';
+
+    for (const symbol of [
+      {
+        metadataField: 'imports',
+        symbolName: 'ApiTokenInterceptorModule',
+        importText: 'ApiTokenInterceptorModule.forRoot()',
+      },
+      {
+        metadataField: 'providers',
+        symbolName: 'API_URL_REGEX',
+        importText: '{ provide: API_URL_REGEX, useValue: /^/ },',
+      },
+      {
+        metadataField: 'providers',
+        symbolName: 'BEARER_TOKEN_CALLBACK_FN',
+        importText:
+          "{ provide: BEARER_TOKEN_CALLBACK_FN, useValue: (): string => 'sampleToken', },",
+      },
+    ]) {
+      applyToUpdateRecorder(
+        recorder,
+        addSymbolToNgModuleMetadata(
+          sourceFile,
+          appModulePath,
+          symbol.metadataField,
+          symbol.symbolName,
+          importPath,
+          symbol.importText
+        )
+      );
+    }
+
+    tree.commitUpdate(recorder);
 
     return tree;
   };
-}
-
-function addModuleAndInjectionTokens(
-  tree: Tree,
-  context: SchematicContext
-): void {
-  context.logger.info('Adding ApiTokenInterceptorModule to the app...');
-  const appModulePath = '/src/app/app.module.ts';
-
-  if (!tree.exists(appModulePath)) {
-    throw new SchematicsException(`The file ${appModulePath} doesn't exist...`);
-  }
-
-  const recorder = tree.beginUpdate(appModulePath);
-  const appModuleFileContent = tree.read(appModulePath);
-
-  if (appModuleFileContent === null) {
-    throw new SchematicsException(
-      `The content of ${appModulePath} couldn't be read...`
-    );
-  }
-
-  const sourceFile = ts.createSourceFile(
-    appModulePath,
-    appModuleFileContent.toString(),
-    ts.ScriptTarget.ES2020
-  );
-  const importPath = '@ngx-toolset/api-token-interceptor';
-
-  for (const symbol of [
-    {
-      metadataField: 'imports',
-      symbolName: 'ApiTokenInterceptorModule',
-      importText: 'ApiTokenInterceptorModule.forRoot()',
-    },
-    {
-      metadataField: 'providers',
-      symbolName: 'API_URL_REGEX',
-      importText:
-        '{ provide: API_URL_REGEX, useValue: /^https://sample-regex.com/ }',
-    },
-    {
-      metadataField: 'providers',
-      symbolName: 'BEARER_TOKEN_CALLBACK_FN',
-      importText:
-        "{ provide: BEARER_TOKEN_CALLBACK_FN, useValue: (): string => 'sampleToken', }",
-    },
-  ]) {
-    applyToUpdateRecorder(
-      recorder,
-      addSymbolToNgModuleMetadata(
-        sourceFile,
-        appModulePath,
-        symbol.metadataField,
-        symbol.symbolName,
-        importPath,
-        symbol.importText
-      )
-    );
-  }
-
-  tree.commitUpdate(recorder);
-}
-
-function installDependencies(context: SchematicContext): void {
-  context.logger.info('Installing dependencies...');
-  context.addTask(new NodePackageInstallTask());
 }
