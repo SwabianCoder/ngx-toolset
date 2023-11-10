aad5e676811a8462a29f52a58f952c1865d15283
SwabianCoder <bernhard@huber-musik.de>
2023-11-10 10:36:55 +0100

    🚨 Ran linter for api token interceptor project

--- projects/api-token-interceptor/src/lib/api-token.interceptor.ts
+++ projects/api-token-interceptor/src/lib/api-token.interceptor.ts
@@ -5,10 +5,7 @@ import {
 } from '@angular/common/http';
 import { inject } from '@angular/core';
 
-import {
-  API_URL_REGEX,
-  BEARER_TOKEN_CALLBACK_FN,
-} from './injection-tokens';
+import { API_URL_REGEX, BEARER_TOKEN_CALLBACK_FN } from './injection-tokens';
 
 /**
  * The apiTokenInterceptor is responsible for adding an Authorization HTTP header to HTTP requests sent via {@link https://angular.io/guide/http Angular HttpClient} if their request URL matches the {@link https://github.com/SwabianCoder/ngx-toolset/blob/main/projects/api-token-interceptor/src/lib/injection-tokens/api-url-regex.ts API_URL_REGEX} value.
@@ -16,7 +13,10 @@ import {
  * @param {HttpRequest<unknown>} req - An HTTP request
  * @param {HttpHandlerFn} next - An HTTP handler function
  */
-export const apiTokenInterceptor: HttpInterceptorFn = (req:HttpRequest<unknown>, next:HttpHandlerFn) => {
+export const apiTokenInterceptor: HttpInterceptorFn = (
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) => {
   const apiUrlRegex = inject(API_URL_REGEX);
   const bearerTokenCallback = inject(BEARER_TOKEN_CALLBACK_FN);
   const urlMatch = apiUrlRegex.test(req.url);
